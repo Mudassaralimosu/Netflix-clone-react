6d254a906d7f9d00642459e8d6b2a5b048831c8e
Mosu <71853471+Mudassaralimosu@users.noreply.github.com>
2021-09-19 19:54:44 +0530

    react movie app

---
+++ Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Nav.css"
+
+function Nav() {
+    return (
+        <div className="nav">
+        <img
+          className="nav__logo"
+          src=""
+          alt="ML icon"
+        />
+        <img
+          className="nav__logo"
+          src=""
+          alt="ML icon"
+        />
+        </div>
+    )
+}
+
+export default Nav
